7c540af2f6d44b6673eb260afdd8eef19968e198
md sanowar alam <atifreza72ar@gmail.com>
2022-08-26 22:43:55 +0530

    oops added

---
+++ abdul bari/oops/abstraction.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+
+class rectangle
+{
+private:
+    /* data */
+    int l,b;
+public:
+
+ void setl(int x){
+     if(x<0){
+         l=1;
+     }else{
+     l=x;
+     }
+ }
+
+ void setb(int y){
+     if(y<0){
+         b=1;
+     }else{
+     b= y;
+     }
+ }
+
+ int getl(){
+     return l;
+ }
+
+ int getb(){
+     return b;
+ }
+
+   int area(){
+       return l*b;
+   }
+   int perimeter(){
+       return 2*(l+b);
+   }
+};
+
+
+
+
+int main(){
+
+   rectangle r1;
+
+
+   r1.setl(10);
+   r1.setb(4);
+
+   cout<<"area of r1 "<<r1.area()<<endl;
+
+   cout<<"r1 detail "<<r1.getl()<<"  "<<r1.getb()<<endl;
+
+
+
+
+
+    return 0;
+
+}
---
+++ abdul bari/sumofnatural.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the value of n\n";
+    cin>>n;
+    float sum;
+    sum= ((float)n*(n+1))/2;
+    cout<<"sum of n natural is: "<<sum;
+
+    return 0;
+}
